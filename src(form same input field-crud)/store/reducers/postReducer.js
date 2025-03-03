81d6ad1f772c7264594583b480946c97e013fa2b
kumaragurumkce <98508552+kumaragurumkce@users.noreply.github.com>
2025-03-03 13:06:03 +0530

    Update postReducer.js

--- src(form same input field-crud)/store/reducers/postReducer.js
+++ src(form same input field-crud)/store/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, ADD_POST, DELETE_POST, EDIT_POST, SET_EDIT_POST } from '../actions/postActions';
+import { FETCH_POSTS, ADD_POST, DELETE_POST, EDIT_POST, SET_EDIT_POST, CANCEL_EDIT } from '../actions/postActions';
 
 const initialState = {
   posts: [],
@@ -19,10 +19,13 @@ const postReducer = (state = initialState, action) => {
         posts: state.posts.map((post) =>
           post.id === action.payload.id ? action.payload : post
         ),
-        editPost: null,
+      
       };
     case SET_EDIT_POST:
       return { ...state, editPost: action.payload };
+    case CANCEL_EDIT:
+      return { ...state, editPost: null };
+
     default:
       return state;
   }
