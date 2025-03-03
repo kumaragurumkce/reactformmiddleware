8f1bf73c358eb72da5fc1c2deb160d3273a45cfd
kumaragurumkce <98508552+kumaragurumkce@users.noreply.github.com>
2025-03-03 13:06:32 +0530

    Update PostList.jsx

--- src(form same input field-crud)/components/PostList.jsx
+++ src(form same input field-crud)/components/PostList.jsx
@@ -12,11 +12,12 @@ import PostForm from './PostForm';
   }, [dispatch]);
 
   return (
-    <div>
+    <div className='post-list'>
       <PostForm />
       <h2>Posts</h2>
       {posts.map((post) => (
-        <div key={post.id}>
+        <div className='post-item'  key={`${post.id}-${Date.now()}`} >
+{/* key={post.id} */}
           <h3>{post.title}</h3>
           <button onClick={() => dispatch(setEditPost(post))}>Edit</button>
           <button onClick={() => dispatch(deletePost(post.id))}>Delete</button>
